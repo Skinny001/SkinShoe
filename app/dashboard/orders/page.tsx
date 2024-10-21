9758f8514819c314678dfeac2696383041e99ce1
Skinny001 <bamigboyetemitope84@gmail.com>
2024-10-21 21:01:03 +0100

    my first commit

---
+++ app/dashboard/orders/page.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+export default function OrdersPage() {
+    return(
+       <Card>
+        <CardHeader>
+            <CardTitle>Orders</CardTitle>
+            <CardDescription>Recent orders from your store!</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Customer</TableHead>
+                        <TableHead>Type</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Date</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+                            <p className="font-medium">Bamigboye Ayodeji</p>
+                            <p className="hidden md:flex text-sm text-muted-foreground">test@test.com</p>
+                        </TableCell>
+                        <TableCell>Sale</TableCell>
+                        <TableCell>Delivered</TableCell>
+                        <TableCell>2024-08-21</TableCell>
+                        <TableCell className="text-right">#300,000</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </CardContent>
+       </Card>
+    )
+}
